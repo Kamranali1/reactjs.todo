27939ab9a21a266c40a7cdf24fa1999beff9e207
unknown <klashari786786@gmail.com>
2020-12-19 10:31:27 +0500

    First

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View  , TextInput,ScrollView, TouchableOpacity} from 'react-native';
+import Main from './App/components/Main'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main />
   );
 }
 
---
+++ App/components/note.js
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View  , TouchableOpacity} from 'react-native';
+
+export default class Note extends React.Component {
+ render (){
+   return (
+       <View key={this.props.keyval} style={styles.note}>
+         <Text style={styles.notetext}>{this.props.val.date}</Text>
+         <Text style={styles.notetext}>{this.props.val.note}</Text>
+         <TouchableOpacity onPress={this.props.deleteMethod} style={styles.noteDelete}>
+             <Text style={styles.noteDeleteText}>Del</Text>
+         </TouchableOpacity>
+       </View>
+   )
+  };
+} 
+
+const styles = StyleSheet.create({
+  note: {
+   position: 'relative',
+   padding: 20,
+   paddingRight: 100,
+   borderBottomWidth: 2,
+   borderBottomColor: '#ededed'
+  },
+  notetext:{
+      padding:20,
+      borderLeftWidth:10,
+      borderLeftColor:'#E91E63'
+  },
+  noteDelete:{
+      position:"absolute",
+      justifyContent:"center",
+      alignItems: 'center',
+      backgroundColor: "#2980b9",
+      padding:10,
+      top:10,
+      bottom:10,
+      right: 10,
+  },
+  noteDeleteText:{
+      color:"white"
+  }
+
+});
